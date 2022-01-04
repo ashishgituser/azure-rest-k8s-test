09adb1569823b2d79330da1013ec5e3ec542127d
Ashish <ashish.chaudhary008@gmail.com>
2022-01-04 23:07:20 +0530

    Initial commit.

---
+++ src/main/java/com/rest/azurerestk8stest/AzureRestK8sTestApplication.java
@@ -0,0 +1,13 @@
+package com.rest.azurerestk8stest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureRestK8sTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureRestK8sTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rest/azurerestk8stest/AzureRestK8sTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.azurerestk8stest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureRestK8sTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
